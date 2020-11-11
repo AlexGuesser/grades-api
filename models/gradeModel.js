89441116760e8081a1a47e5a19d8192d568cc547
Alex Guesser <alex.guesser@hotmail.com>
2020-11-11 07:57:01 -0300

    Initial commit

---
+++ models/gradeModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const gradeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  subject: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  value: {
+    type: Number,
+    require: true,
+    min: 0,
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const gradeModel = mongoose.model("grades", gradeSchema, "grades");
+
+export { gradeModel };
